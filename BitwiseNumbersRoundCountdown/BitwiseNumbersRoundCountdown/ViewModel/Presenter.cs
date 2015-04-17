92493fedd2db84ee10a48cb77cc3fb38fbaba7f0
Brodie Gilmour <brodie.gilmour@webapplicationsuk.com>
2015-04-17 15:56:16 +0100

    Updated to MVVM pattern

---
+++ BitwiseNumbersRoundCountdown/BitwiseNumbersRoundCountdown/ViewModel/Presenter.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace BitwiseNumbersRoundCountdown.ViewModel
+{
+    public class Presenter : ObservableObject {
+        private static readonly Model.NumbersRound _game = new Model.NumbersRound();
+
+        public Model.NumbersRound Game => _game;
+    }
+}
